4848a05fc44d2882ef965f016ea7545adb37f383
karixdev <30869888+karixdev@users.noreply.github.com>
2022-09-30 23:58:50 +0200

    Created class responsible for courses date and time config

---
+++ src/main/java/org/example/course/CourseDateAndTimeConfig.java
@@ -0,0 +1,7 @@
+package org.example.course;
+
+public class CourseDateAndTimeConfig {
+    // Time
+    public static final int FIRST_CELL_TOP_VALUE = 237;
+    public static final double ONE_HOUR_BLOCK_HEIGHT = 45;
+}
