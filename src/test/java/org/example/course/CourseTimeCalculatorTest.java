03de352d9b3fc224d0e129d18b672eb0310312cd
karixdev <30869888+karixdev@users.noreply.github.com>
2022-09-30 23:57:09 +0200

    Created tests for testing getDay method

--- src/test/java/org/example/course/CourseTimeCalculatorTest.java
+++ src/test/java/org/example/course/CourseTimeCalculatorTest.java
@@ -3,6 +3,7 @@ package org.example.course;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class CourseTimeCalculatorTest {
@@ -12,9 +13,33 @@ public class CourseTimeCalculatorTest {
         int top = 259;
 
         // when
-        LocalTime result = CourseTimeCalculator.getTime(top, 8);
+        LocalTime result = CourseDateAndTimeCalculator.getTime(top, 8);
 
         // then
         Assertions.assertEquals(LocalTime.of(8, 30), result);
+    }
+
+    @Test
+    public void shouldReturnCorrectDayForCourseOnEveryWeek() {
+        // given
+        int left = 88;
+
+        // when
+        DayOfWeek result = CourseDateAndTimeCalculator.getDayOfWeek(left);
+
+        // then
+        Assertions.assertEquals(DayOfWeek.MONDAY, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectDayForCourseOnNotEveryWeek() {
+        // given
+        int left = 337;
+
+        // when
+        DayOfWeek result = CourseDateAndTimeCalculator.getDayOfWeek(left);
+
+        // then
+        Assertions.assertEquals(DayOfWeek.TUESDAY, result);
     }
 }
