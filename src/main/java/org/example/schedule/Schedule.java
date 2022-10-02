bf9292264260cf7c42f200b433869daa8a355328
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-02 18:49:29 +0200

    Added list of school days to schedule

--- src/main/java/org/example/schedule/Schedule.java
+++ src/main/java/org/example/schedule/Schedule.java
@@ -2,12 +2,14 @@ package org.example.schedule;
 
 import org.example.course.Course;
 
+import java.time.DayOfWeek;
 import java.util.List;
+import java.util.Map;
 
 public class Schedule {
     private String id;
 
-    private List<Course> courses;
+    private List<SchoolDay> schoolDays;
 
     public Schedule(String id) {
         this.id = id;
@@ -19,5 +21,13 @@ public class Schedule {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public List<SchoolDay> getSchoolDays() {
+        return schoolDays;
+    }
+
+    public void setSchoolDays(List<SchoolDay> schoolDays) {
+        this.schoolDays = schoolDays;
     }
 }
