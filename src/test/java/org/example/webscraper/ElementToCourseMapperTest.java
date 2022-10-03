a2380656ca7937f95ca4f268fd2532aa24afcbb4
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-03 19:37:18 +0200

    Created test checking if valid element is properly mapped to course

--- src/test/java/org/example/webscraper/ElementToCourseMapperTest.java
+++ src/test/java/org/example/webscraper/ElementToCourseMapperTest.java
@@ -1,10 +1,14 @@
 package org.example.webscraper;
 
+import org.example.course.Course;
+import org.example.course.Weeks;
 import org.example.exception.EmptyCourseElementException;
 import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.Map;
 
 public class ElementToCourseMapperTest {
@@ -22,6 +26,25 @@ public class ElementToCourseMapperTest {
 
         // when and then
         Assertions.assertThrows(EmptyCourseElementException.class, () -> ElementToCourseMapper.map(element, 7));
+    }
+
+    @Test
+    public void shouldReturnCorrectCourseWithValidCredentials() {
+        // given
+        Element element = createValidCourseElement(VALID_TEXT_EXAMPLE, VALID_CW_EXAMPLE, VALID_CH_EXAMPLE, getValidStylesMap());
+
+        // when
+        Course result = ElementToCourseMapper.map(element, 7);
+
+        // then
+        Course target = new Course(
+                VALID_TEXT_EXAMPLE,
+                LocalTime.of(8, 30),
+                LocalTime.of(10, 0),
+                DayOfWeek.WEDNESDAY,
+                Weeks.EVERY
+        );
+        Assertions.assertEquals(target, result);
     }
 
     public static Element createValidCourseElement(String text, String cw, String ch, Map<String, String> stylesMap) {
