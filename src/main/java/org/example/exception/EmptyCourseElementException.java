a5c369841738b84dc6512f0faa04197d74801e63
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-03 19:09:07 +0200

    Created exception empty course element

---
+++ src/main/java/org/example/exception/EmptyCourseElementException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class EmptyCourseElementException extends RuntimeException {
+    public EmptyCourseElementException(String message) {
+        super(message);
+    }
+}
