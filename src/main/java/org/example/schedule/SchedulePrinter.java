d7a577bafe19b313d2821da9c7fa3a67c450bd91
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-04 22:08:39 +0200

    Created schedule printer

---
+++ src/main/java/org/example/schedule/SchedulePrinter.java
@@ -0,0 +1,31 @@
+package org.example.schedule;
+
+import org.example.course.Course;
+
+import java.time.DayOfWeek;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SchedulePrinter {
+    public static void print(Schedule schedule) {
+        List<Course> courses = schedule.getCourses();
+        DayOfWeek lastDayOfWeek = courses.get(0).dayOfWeek();
+
+        for (int i = 0; i < schedule.getCourses().size(); i++) {
+            Course course = courses.get(i);
+
+            if (!lastDayOfWeek.equals(course.dayOfWeek()) || i == 0) {
+                lastDayOfWeek = course.dayOfWeek();
+                System.out.println("-----------");
+                System.out.println(lastDayOfWeek);
+                System.out.println("-----------");
+            }
+
+            System.out.println();
+            System.out.println("Description: " + course.description());
+            System.out.println("Starts at: " + course.startsAt());
+            System.out.println("Ends at: " + course.endsAt());
+            System.out.println();
+        }
+    }
+}
