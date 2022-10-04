b77436c308b2063c3561013ce5a938292023050b
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-04 17:34:04 +0200

    Created test checking if getCourses returns correct list

--- src/test/java/org/example/webscraper/CoursesWebScraperTest.java
+++ src/test/java/org/example/webscraper/CoursesWebScraperTest.java
@@ -1,10 +1,15 @@
 package org.example.webscraper;
 
+import org.example.course.Course;
 import org.example.exception.EmptyTimeCellListException;
+import org.example.helpers.DocumentHelper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 public class CoursesWebScraperTest {
     @Test
@@ -30,5 +35,31 @@ public class CoursesWebScraperTest {
 
         // then
         Assertions.assertEquals(7, result);
+    }
+
+    @Test
+    public void shouldReturnEmptyListWithNoCourseElements() {
+        // given
+        Document document = Jsoup.parse("""
+                <div class="cd">07:00-08:00</div>
+                """);
+
+        // when
+        List<Course> result = CoursesWebScraper.getCourses(document);
+
+        // then
+        Assertions.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void shouldReturnCorrectListWithProvidedDocument() throws IOException {
+        // given
+        Document document = DocumentHelper.loadDocumentFromFile();
+
+        // when
+        List<Course> result = CoursesWebScraper.getCourses(document);
+
+        // then
+        Assertions.assertEquals(DocumentHelper.getCoursesFromExampleFile(), result);
     }
 }
