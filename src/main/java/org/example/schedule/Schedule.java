9872ecb8bbc3169f81a47f70c4c57228035dcc44
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-04 22:15:33 +0200

    Implemented download method

--- src/main/java/org/example/schedule/Schedule.java
+++ src/main/java/org/example/schedule/Schedule.java
@@ -2,30 +2,32 @@ package org.example.schedule;
 
 import org.example.course.Course;
 import org.example.url.Url;
+import org.example.webscraper.CoursesWebScraper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
-import java.time.DayOfWeek;
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class Schedule {
     private String id;
 
-    private List<SchoolDay> schoolDays;
+    private List<Course> courses;
 
     public Schedule(String id) {
         this.id = id;
     }
 
-    public void download() {
-        // create url
+    public void download() throws IOException {
+        ScheduleUrlBuilder urlBuilder = new ScheduleUrlBuilder(id);
+        Url url = urlBuilder.build();
 
-        // web scrape
+        Document document = Jsoup.connect(url.toString()).get();
 
-        // create list of course
-
-        // group courses by day of week
-
-        // add courses to schoolDays list
+        courses = CoursesWebScraper.getCourses(document)
+                .stream()
+                .sorted()
+                .toList();
     }
 
     public String getId() {
@@ -36,11 +38,7 @@ public class Schedule {
         this.id = id;
     }
 
-    public List<SchoolDay> getSchoolDays() {
-        return schoolDays;
-    }
-
-    public void setSchoolDays(List<SchoolDay> schoolDays) {
-        this.schoolDays = schoolDays;
+    public List<Course> getCourses() {
+        return courses;
     }
 }
