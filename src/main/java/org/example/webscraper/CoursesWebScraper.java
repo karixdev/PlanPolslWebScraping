e7b7f0d04cb2e752c32c0b17a79eefec7f895d71
karixdev <kari.klucz@gmail.com>
2022-10-21 22:50:36 +0200

    Changed CoursesWebScraper to not use static methods

--- src/main/java/org/example/webscraper/CoursesWebScraper.java
+++ src/main/java/org/example/webscraper/CoursesWebScraper.java
@@ -1,5 +1,7 @@
 package org.example.webscraper;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.example.course.Course;
 import org.example.exception.EmptyTimeCellListException;
 import org.jsoup.nodes.Document;
@@ -10,30 +12,18 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class CoursesWebScraper {
-
     private final static String COURSE_ELEMENT_CLASS = "coursediv";
-    private final static String TIME_CELL_CLASS = "cd";
 
-    public static int getScheduleStartTime(Document document) {
-        List<Element> timeCellsList = document.getElementsByClass(TIME_CELL_CLASS).stream()
-                .filter(element -> TimeCellUtils.isValidTimeCell(element.text()))
-                .toList();
+    private ElementToCourseMapper elementToCourseMapper;
 
-        if (timeCellsList.size() == 0) {
-            throw new EmptyTimeCellListException("Could not find any time cell");
-        }
-
-        return LocalTime.parse(timeCellsList.get(0).text().split("-")[0]).getHour();
-    }
-
-    public static List<Course> getCourses(Document document) {
-        int startTime = getScheduleStartTime(document);
-
+    public List<Course> getCourses(Document document) {
         Elements coursesElements = document.getElementsByClass(COURSE_ELEMENT_CLASS);
 
         return coursesElements.stream()
-                .map(element -> ElementToCourseMapper.map(element, startTime))
+                .map(elementToCourseMapper::map)
                 .filter(Objects::nonNull)
                 .toList();
     }
--- src/test/java/org/example/webscraper/CoursesWebScraperTest.java
+++ src/test/java/org/example/webscraper/CoursesWebScraperTest.java
@@ -1,40 +1,23 @@
 package org.example.webscraper;
 
 import org.example.course.Course;
-import org.example.exception.EmptyTimeCellListException;
 import org.example.helpers.DocumentHelper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
 public class CoursesWebScraperTest {
-    @Test
-    public void shouldThrowExceptionWhenNotFoundAnyTimeCell() {
-        // given
-        Document document = Jsoup.parse("");
+    private CoursesWebScraper coursesWebScraper;
 
-        // when and then
-        Assertions.assertThrows(EmptyTimeCellListException.class, () -> CoursesWebScraper.getScheduleStartTime(document));
-    }
-
-    @Test
-    public void shouldReturnCorrectScheduleTimeStartProvidedValidDocument() {
-        // given
-        Document document = Jsoup.parse("""
-                <div class="cd">07:00-08:00</div>
-                <div class="cd">13:00-14:00</div>
-                <div class="cd">20:00-21:00</div>
-                """);
-
-        // when
-        int result = CoursesWebScraper.getScheduleStartTime(document);
-
-        // then
-        Assertions.assertEquals(7, result);
+    @BeforeEach
+    public void setup() {
+        coursesWebScraper = new CoursesWebScraper(
+                new ElementToCourseMapper(7));
     }
 
     @Test
@@ -45,7 +28,7 @@ public class CoursesWebScraperTest {
                 """);
 
         // when
-        List<Course> result = CoursesWebScraper.getCourses(document);
+        List<Course> result = coursesWebScraper.getCourses(document);
 
         // then
         Assertions.assertEquals(0, result.size());
@@ -57,7 +40,7 @@ public class CoursesWebScraperTest {
         Document document = DocumentHelper.loadDocumentFromFile();
 
         // when
-        List<Course> result = CoursesWebScraper.getCourses(document);
+        List<Course> result = coursesWebScraper.getCourses(document);
 
         // then
         Assertions.assertEquals(DocumentHelper.getCoursesFromExampleFile(), result);
