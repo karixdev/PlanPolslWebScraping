90fa77e820e6924dcb32b877f29f5bc65575b8f8
karixdev <kari.klucz@gmail.com>
2022-10-21 21:55:39 +0200

    Removed replaced url boilerplate code with lombok

--- src/main/java/org/example/url/Url.java
+++ src/main/java/org/example/url/Url.java
@@ -1,12 +1,18 @@
 package org.example.url;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Url {
     public static final String BASE_URL = "https://plan.polsl.pl";
-
     private String path;
     private Set<QueryParameter> queryParameters;
 
@@ -18,27 +24,6 @@ public class Url {
     @Override
     public String toString() {
         return BASE_URL + "/" + path + UrlUtils.SetOfQueryParametersToString(queryParameters);
-    }
-
-    public Url addQueryParameter(QueryParameter queryParameter) {
-        queryParameters.add(queryParameter);
-        return this;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public Set<QueryParameter> getQueryParameters() {
-        return queryParameters;
-    }
-
-    public void setQueryParameters(Set<QueryParameter> queryParameters) {
-        this.queryParameters = queryParameters;
     }
 
     public static class Builder {
@@ -58,18 +43,5 @@ public class Url {
         public Url build() {
             return new Url(this);
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Url url = (Url) o;
-        return Objects.equals(path, url.path) && Objects.equals(queryParameters, url.queryParameters);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(path, queryParameters);
     }
 }
