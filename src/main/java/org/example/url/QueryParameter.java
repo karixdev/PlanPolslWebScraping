a56f2385f8467ff8259fd44504862caa018faedd
karixdev <kari.klucz@gmail.com>
2022-10-21 21:50:26 +0200

    Changed query parameter to be record

--- src/main/java/org/example/url/QueryParameter.java
+++ src/main/java/org/example/url/QueryParameter.java
@@ -2,46 +2,9 @@ package org.example.url;
 
 import java.util.Objects;
 
-public class QueryParameter {
-    private String key;
-    private String value;
-
-    public QueryParameter(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
+public record QueryParameter(String key, String value) {
     @Override
     public String toString() {
         return key + "=" + value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        QueryParameter that = (QueryParameter) o;
-        return Objects.equals(key, that.key) && Objects.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key, value);
     }
 }
